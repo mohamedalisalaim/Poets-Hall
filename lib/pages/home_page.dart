6a8924221afa8773bb24da6e52d267247160f4d9
mohamedalisalaim <fury45409@gmail.com>
2024-11-30 14:17:29 +0200

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,21 +2,20 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:poets_hall/models/poet.dart';
-import 'package:poets_hall/util/poet_tile.dart';
 import 'package:http/http.dart' as http;
 
+// ignore: must_be_immutable
 class HomePage extends StatelessWidget {
   HomePage({super.key});
 
   List<Poet> poets = [];
 
   Future getPoets() async {
-    poets.clear();
     var response = await http.get(Uri.https("poetrydb.org", "author"));
     var jsonData = jsonDecode(response.body);
 
-    for (String eachPoet in jsonData["authors"]) {
-      var poet = Poet(name: eachPoet);
+    for (var eachPoet in jsonData["authors"]) {
+      final poet = Poet(name: eachPoet);
       poets.add(poet);
     }
   }
@@ -24,27 +23,25 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: RefreshIndicator(
-        onRefresh: getPoets,
-        child: FutureBuilder(
-          future: getPoets(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return ListView.builder(
-                itemCount: poets.length,
-                itemBuilder: (context, index) {
-                  return PoetTile(name: poets[index].name);
-                },
-              );
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
-        ),
-      )),
+      body: FutureBuilder(
+        future: getPoets(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return ListView.builder(
+              itemCount: poets.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(poets[index].name),
+                );
+              },
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
     );
   }
 }
